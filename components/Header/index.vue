b7a6e81da2df317e4287bf90121f9599e3d25dfe
Artem <myartoym@gmail.com>
2024-07-31 00:56:44 +0300

    header

---
+++ components/Header/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <img src="~/assets/img/logo.webp" alt="" width="140" height="50px">
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+        </ul>
+
+    </nav>
+    <div class="icons">-</div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+    header {
+        display: flex;
+        align-items: center;
+        background-color: #eee;
+        height: 50px;
+        justify-content: space-around;
+
+        & img {
+            object-fit: cover;
+            object-position: center;
+        }
+    }
+    ul {
+        display: flex;
+        gap: 12px;
+
+        & li {
+            & a {
+                color: #999;
+            }
+        }
+    }
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    body
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
