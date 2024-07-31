fb8c54d53b5439273868e674714da37ef57b5ab3
Artem <myartoym@gmail.com>
2024-07-31 11:56:14 +0300

    install sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"]
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 'tab',
+        semi: true,
+      }
+    }
+  }
 })
