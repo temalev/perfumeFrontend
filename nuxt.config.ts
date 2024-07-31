40ee9e7ebf4b4f076fe3baab151c0f9ad053087f
Artem <myartoym@gmail.com>
2024-07-31 15:01:14 +0300

    style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
+	css: ['~/assets/styles/main.css', '~/assets/styles/common.scss'],
 	modules: ['@nuxt/eslint'],
 	eslint: {
 		config: {
