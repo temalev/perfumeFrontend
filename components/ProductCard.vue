420db5976dc82f9409c8f4caae6b34c0e1ded81b
Artem <myartoym@gmail.com>
2024-08-11 23:20:06 +0300

    Доработал карточки товара

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="card d-flex-column">
-    <div class="img d-flex j-c">
+    <div class="img-container d-flex j-c">
       <img :src="image(imageUrl)" :alt="data.name" />
+      <button class="ico-btn d-flex align-center j-c">
+        <Icon name="fa6-regular:heart" style="font-size: 20px" />
+      </button>
     </div>
 
-    <div class="info">
-      <div class="info-name d-flex-column align-flex-start">
-        <span class="category">{{ data.category }}</span>
-        <span class="name">{{ data.name }}</span>
-      </div>
+    <div class="info d-flex-column align-flex-start gap-3">
+      <span class="category">{{ data.type }}</span>
+      <span class="name">{{ data.name }}</span>
 
-      <div class="pay d-flex-row j-sb">
+      <div class="pay d-flex-row j-sb w100">
         <div class="info-price d-flex align-flex-end">
-          <span class="new-price">{{ data.price }}</span>
-          <span v-if="data.oldPrice" class="old-price">{{
-            data.oldPrice
-          }}</span>
+          <span class="new-price">{{ data.price }}&nbsp;₽</span>
+          <span v-if="data.basePrice" class="old-price">
+            {{ data.basePrice }}&nbsp;₽
+          </span>
         </div>
 
-        <div class="btn-pay">
-          <button class="add-to-cart d-flex align-center j-c">+</button>
-        </div>
+        <button class="ico-btn d-flex align-center j-c">
+          <Icon name="heroicons-solid:plus-sm" style="font-size: 20px" />
+          <Icon name="ph:shopping-cart-simple-bold" style="font-size: 20px" />
+        </button>
       </div>
     </div>
   </div>
@@ -42,7 +44,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .card {
   width: 300px;
   box-shadow: 0px 0px 20px 2px rgba(108, 108, 108, 0.309);
@@ -55,6 +57,16 @@ img {
   left: 76px;
   background-color: #999;
 }
+
+.img-container {
+  position: relative;
+  & .ico-btn {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+}
+
 .product-price {
   margin-bottom: 1rem;
 }
@@ -62,14 +74,14 @@ img {
   text-decoration: line-through;
   color: #999;
   font-family: Anselm Sans;
-  font-size: 12px;
+  font-size: 16px;
   font-weight: 400;
   line-height: 15.43px;
   text-align: center;
 }
 .new-price {
   font-family: Anselm Sans;
-  font-size: 12px;
+  font-size: 16px;
   font-weight: 400;
   line-height: 15.43px;
   text-align: center;
@@ -80,20 +92,20 @@ img {
 .category {
   color: #999;
   font-family: Anselm Sans;
-  font-size: 11px;
+  font-size: 16px;
   font-weight: 400;
   line-height: 14.15px;
   text-align: center;
 }
 .name {
   font-family: Anselm Sans;
-  font-size: 14px;
+  font-size: 16px;
   font-weight: 400;
   line-height: 18px;
   text-align: center;
 }
 .info {
-  padding: 1rem;
+  padding: 20px;
 }
 
 .info-price {
