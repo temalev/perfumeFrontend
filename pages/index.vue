b9cb9995ecad06b635a7d277c465f36308676d74
Artem <myartoym@gmail.com>
2024-08-12 16:54:56 +0300

    fix video

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@
         autoplay
         loop
         muted
-        controls="true"
         style="height: 600px; width: 100%; object-fit: cover"
       >
         <source src="/video/bg.webm" type="video/webM" />
