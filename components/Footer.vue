3eb10fe75856c8528013220debd4c9ad37413919
Artem <myartoym@gmail.com>
2024-09-18 20:41:36 +0300

    Данные

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,6 +8,30 @@
         </li>
       </ul>
     </div>
+    <div class="d-flex-column">
+      <div>
+        <el-link>info@parfburo.com</el-link>
+        - По вопросам сотрудничества, идеям по улучшению и жалобам.
+      </div>
+
+      <ul>
+        <li>
+          <el-text class="mx-1" type="info" size="small"
+            >ИНДИВИДУАЛЬНЫЙ ПРЕДПРИНИМАТЕЛЬ ДЁМИН МИХАИЛ МИХАЙЛОВИЧ</el-text
+          >
+        </li>
+        <li>
+          <el-text class="mx-1" type="info" size="small"
+            >ИНН: 621504323474</el-text
+          >
+        </li>
+        <li>
+          <el-text class="mx-1" type="info" size="small"
+            >ОГРНИП: 321623400003805</el-text
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,6 +46,7 @@ export default {};
   background-color: #000;
   color: #fff;
   padding: 20px;
+  justify-content: space-around;
 
   & .pointer:hover {
     opacity: 0.8;
