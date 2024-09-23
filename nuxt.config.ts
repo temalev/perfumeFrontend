cd5bfb9aabc8a70293cd48285cdcf9fe62d83875
Artem <myartoym@gmail.com>
2024-09-23 16:03:17 +0300

    вывод нот

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000
-  },
   css: ['~/assets/styles/main.scss', '~/assets/styles/common.scss'],
   vite: {
     css: {
