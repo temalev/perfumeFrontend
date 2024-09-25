101acc6e2b993b888b72550e5c81ebb8be4b2ab0
Artem <myartoym@gmail.com>
2024-09-25 14:48:56 +0300

    storis

--- pages/index.vue
+++ pages/index.vue
@@ -26,11 +26,10 @@
         v-for="item in media"
         :key="item.id"
         :data="item"
-        ref="video"
         playsinline
         loop
         muted
-        style="height: 600px; width: 100%; object-fit: cover"
+        @click="openedVideo = item"
       >
         <source :src="item.url" type="video/mp4" />
       </video>
@@ -55,6 +54,18 @@
         />
       </div>
     </div>
+    <modal v-if="openedVideo" @close="openedVideo = null">
+      <video
+        :data="openedVideo"
+        playsinline
+        loop
+        autoplay
+        muted
+        style="width: 100%; height: 100%"
+      >
+        <source :src="openedVideo.url" type="video/mp4" />
+      </video>
+    </modal>
   </div>
 </template>
 
@@ -64,9 +75,10 @@ import ProductCard from '~/components/ProductCard.vue';
 import MediaCard from '~/components/MediaCard.vue';
 
 import { getProducts } from '@/api/productApi.js';
+import Modal from '~/components/ui/Modal.vue';
 
 export default {
-  components: { ProductCard, MediaCard },
+  components: { ProductCard, MediaCard, Modal },
   data() {
     return {
       products: products,
@@ -86,17 +98,10 @@ export default {
           url: 'https://841301.selcdn.ru/rkTech/perfume/files/312323398_An_hvv49_8TEFfGLgPlj2VgFEdmixlO0wnacygRW1BFZSlL_0XdxDNpwe2.mp4',
         },
       ],
+      openedVideo: null,
     };
   },
   mounted() {
-    const video = this.$refs.video;
-
-    if (video[0].offsetLeft === 0) {
-      video[0].play();
-    }
-    // video.play();
-    console.log(video);
-
     this.getProductsHit();
     this.getProductsSale();
   },
@@ -186,9 +191,17 @@ export default {
     overflow: scroll;
     scroll-behavior: smooth;
     scroll-snap-type: x mandatory;
+    margin: 20px;
+    gap: 12px;
 
     & video {
       scroll-snap-align: start;
+      border-radius: 22px;
+      border: 1px solid #2a4d84;
+      padding: 2px;
+      height: 200px;
+      width: 200px;
+      object-fit: cover;
     }
   }
 }
