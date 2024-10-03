b84856bc850c7ebc74589e79a70ef3a5a947a44a
Artem <myartoym@gmail.com>
2024-10-03 12:42:16 +0300

    Поиск

---
+++ components/ui/SearchModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="brands-modal">
+    <button class="ico-btn m-4" @click="$emit('close')">
+      <Icon name="material-symbols-light:close" style="font-size: 40px" />
+    </button>
+    <div class="brands-modal-content">
+      <el-input v-model="search" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.brands-modal {
+  width: 100%;
+  height: calc(100% - 50px);
+  background-color: #fff;
+  position: fixed;
+  top: 50px;
+  left: 0;
+  z-index: 9999;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  &-content {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 22px;
+    padding: 22px;
+    align-content: flex-start;
+    overflow: auto;
+  }
+}
+
+.column {
+  height: fit-content;
+  flex-grow: 1;
+}
+
+.brands {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.brand {
+  transition: 0.2s;
+  &:hover {
+    color: $color-light;
+  }
+}
+</style>
